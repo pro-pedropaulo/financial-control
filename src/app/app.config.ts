6f3f6a67cd6625f946d7208a0889f34dbe4644f6
Pedro Silva <pedro.silva@b2ml.com.br>
2024-05-27 20:39:25 -0300

    build: Add ngx-mask provider to app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()],
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideEnvironmentNgxMask(),
+  ],
 };
